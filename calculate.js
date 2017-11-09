c1d928a582c489f2815cd558b8dd0863b3eb7cbe
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-11-09 12:34:47 -0800

    abandoning pemdas

--- calculate.js
+++ calculate.js
@@ -1,108 +1,113 @@
-class Queue {
-  constructor() {
-    this.storage = [];
-  }
+// class Queue {
+//   constructor() {
+//     this.storage = [];
+//   }
 
-  push(val) {
-    this.storage.unshift(val);
-  }
+//   push(val) {
+//     this.storage.unshift(val);
+//   }
 
-  pop() {
-    return this.storage.pop();
-  }
+//   pop() {
+//     return this.storage.pop();
+//   }
 
-  peek() {
-    return this.storage[this.storage.length - 1];
-  }
+//   peek() {
+//     return this.storage[this.storage.length - 1];
+//   }
 
-  isEmpty() {
-    return this.storage.length === 0;
-  }
+//   isEmpty() {
+//     return this.storage.length === 0;
+//   }
 
-  size() {
-    return this.storage.length;
-  }
-}
+//   size() {
+//     return this.storage.length;
+//   }
+// }
 
-class Stack {
-  constructor() {
-    this.storage = [];
-  }
+// class Stack {
+//   constructor() {
+//     this.storage = [];
+//   }
 
-  push(val) {
-    this.storage.push(val);
-  }
+//   push(val) {
+//     this.storage.push(val);
+//   }
 
-  pop() {
-    return this.storage.pop();
-  }
+//   pop() {
+//     return this.storage.pop();
+//   }
 
-  peek() {
-    return this.storage[this.storage.length - 1];
-  }
+//   peek() {
+//     return this.storage[this.storage.length - 1];
+//   }
 
-  isEmpty() {
-    return this.storage.length === 0;
-  }
+//   isEmpty() {
+//     return this.storage.length === 0;
+//   }
 
-  size() {
-    return this.storage.length;
-  }
-}
+//   size() {
+//     return this.storage.length;
+//   }
+// }
+
+  // var pemdas = {
+  //   '*': 4,
+  //   '/': 3,
+  //   '+': 2,
+  //   '-': 1,
+  // };
 
 var calculate = (s) => {
-  var pemdas = {
-    '*': 4,
-    '/': 3,
-    '+': 2,
-    '-': 1,
-  }
+  s = s.replace(/\s/g, '');
+
+  var pemdas = ['*', '/', '+', '-'];
 
   const doMath = (a, b, operator) => {
-    // console.log('AAA', a)
-    // console.log('BBB', b)
-    if (!a || !b || !operator) return a || b;
-    else if (pemdas[operator] === 4) return a * b;
-    else if (pemdas[operator] === 3) return a / b;
-    else if (pemdas[operator] === 2) return a + b;
+    if (typeof a === 'string') a = parseInt(a);
+    if (typeof b === 'string') b = parseInt(b);
+    if (operator === '*') return a * b;
+    else if (operator === '/') return a / b;
+    else if (operator === '+') return a + b;
     else return a - b;
   }
 
-  var numStack = new Stack();
-  var operStack = new Stack();
-  var lastOperator = 0;
 
-  while (s.length) {
-    var val = s[0];
-    // console.log('val', val)
-
-    if (pemdas[val] && operStack.isEmpty()) { // if it's an operator
-      operStack.push(val);
-
-
-
-    } else {
-      numStack.push(parseInt(val));
-    }
-    // evaluate and push result into numStack
-    var b = numStack.pop();
-    var a = numStack.pop();
-    console.log('a', a)
-    console.log('b', b)
-    var operator = operStack.pop();
-    console.log('operator', operator)
-    var res = doMath(a, b, operator);
-    console.log('res', res)
-    numStack.push(res);
-
-
-    s = s.slice(1);
-    s = s.trim();
+  var nums = s.replace(/[-\*\+\/]/g, '  ').split(' ')
+  var ops = s.replace(/[0-9]/g, ' ').split(''); /*replace(/[0-9]/g, '')*/
+  // console.log('nums', nums)
+  // console.log('ops', ops)
+  while (nums.length > 1) {
+    // pemdas.forEach(operator => {
+      ops.forEach((char, i) => {
+        if (pemdas.includes(char)) {
+          var a = nums[i - 1];
+          var b = nums[i + 1];
+          var res = doMath(a, b, char);
+          // console.log('a', a)
+          // console.log('b', b)
+          // console.log('res', res)
+          nums.splice(i - 1, 3, res);
+          ops.splice(i - 1, 2)
+          // console.log('ops', ops)
+          // console.log('nums', nums)
+        }
+      })
+    // })
   }
-  console.log(numStack)
+
+  console.log('result: ', parseInt(nums.join('')));
+  return parseInt(nums.join(''));
 
 };
 
-var s = '1 + 1';
-// console.log(calculate(s));
-console.log(calculate('2 + 6 / 3'))
+
+// NEED TO ROUND DOWN, DOESN'T CARE ABOUT PEMDAS
+
+// console.log(calculate('1 + 1') === 2);
+// console.log(calculate('2 + 6 / 3') === 4)
+// console.log(calculate('42') === 42)
+// console.log(calculate('0-2147483647') === -2147483647)
+// console.log(calculate('1+1+1') === 3)
+// console.log(calculate('1-1+1') === 1)
+// console.log(eval('1-1/2'))
+console.log(eval('3-2/2'))
