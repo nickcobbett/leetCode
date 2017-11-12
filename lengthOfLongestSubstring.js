331ba17f556b923b695b7fe5c2b0cb4e0772d942
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-11-11 16:25:09 -0800

    cleaned up

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -11,49 +11,26 @@ var lengthOfLongestSubstring = function(s) {
       count++;
     } else {
       var newStrIndex = chars[char];
-      // console.log('i', i);
-      // console.log('dup', char);
-      // console.log('chars before delete', chars);
-      // console.log(newStrIndex)
-      // console.log('startIndex', startIndex)
 
-      if (startIndex === 0 && chars[char] === 0) {
-        delete chars[s[0]];
-      }
-      if (startIndex === 0 && chars[char] === 1) {
-        delete chars[s[0]];
-        delete chars[s[1]];
-      }
-      // delete chars[char];
       for (var k = startIndex; k < newStrIndex; k++) {
-      // for (var k = 0; k < newStrIndex - 1; k++) {
-        // console.log('k', k)
-        // console.log('deleted char: ', s[k])
         if (k === chars[s[k]]) {
           delete chars[s[k]];
         }
       }
 
-      // console.log('chars after delete', chars);
-      // console.log('count', count);
-      // console.log('new count', i - newStrIndex)
       maxCount = maxCount > count ? maxCount : count;
-      count = i - newStrIndex;
+      count = i - newStrIndex; //update the count
       startIndex = newStrIndex - 1; // update startIndex
       chars[char] = i; // update the index of the char in chars
     }
   }
-  // console.log('maxCount: ', maxCount);
-  // console.log('last count: ', count);
   return maxCount > count ? maxCount : count;
-
 };
 
 module.exports = lengthOfLongestSubstring;
 
 // console.log(lengthOfLongestSubstring('abcabcbb') === 3); // 'abc'
-console.log(lengthOfLongestSubstring("oiholrxmihbftoarawdazeoubedgtkpityygpvvafwfymgsmcvodbexd") === 11)
-
+// console.log(lengthOfLongestSubstring("oiholrxmihbftoarawdazeoubedgtkpityygpvvafwfymgsmcvodbexd") === 11)
 // console.log(lengthOfLongestSubstring("eeydgwdykpv") === 7)
 // console.log(lengthOfLongestSubstring("abba") === 2)
 // console.log(lengthOfLongestSubstring("bziuwnklhqzrxnb") === 11) // 'iuwnklhqzrx'
