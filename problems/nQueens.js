4f93ca508603b26ce14fa780286124511db5d724
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-11-14 15:01:07 -0800

    added some test cases

--- problems/nQueens.js
+++ problems/nQueens.js
@@ -26,10 +26,7 @@ class Board {
   }
 
   checkDiagonal(row, col) {
-    // row = parseInt(row);
-    // col = parseInt(col)
     for (var i = 0; i < this.queens.length; i++) {
-      // console.log(Math.abs((row - this.queens[i][0]) / (col - this.queens[i][1])))
       if (Math.abs((parseInt(row) - this.queens[i][0]) / (parseInt(col) - this.queens[i][1])) === 1) {
         return false;
       }
@@ -67,39 +64,54 @@ class Board {
 var solveNQueens = function(n) {
   var boards = [];
   var boardsHash = {};
+
+  var fillBoard = (board, row, col, visited) => {
+
+    // place queens in a board
+
+    // have a matrix of all cells
+
+    // call this function for each cell. each time you add a queen you add that to the visited and you make that visited cell unavailable the first time it's attempted to be visited
+  }
+
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
-      var numTries = 0;
       var board = new Board(n, i, j);
-      var count = 1;
-      var queens = board.queens;
-      // console.log('###', queens)
+      // console.log(board)
       var secondQueen = board.placeNextQueen();
-      var lastQueen = board.placeNextQueen();
-      queens.push(lastQueen);
 
-      // fill board completely, then reset board and reset last queen (this is likely the problem)
-      while (numTries < n * n) {
-      console.log(board.queens)
-        while (board.placeNextQueen()) {
-          board.placeNextQueen();
-        }
-        count = board.getNQueens();
-        if (count === n && !boardsHash[JSON.stringify(board.getBoard())]) {
-          boards.push(board.getBoard());
-          boardsHash[JSON.stringify(board.getBoard())] = 1;
-        }
-        board = new Board(n, i, j) // reset board
-        secondQueen = board.placeNextQueen(secondQueen[0], secondQueen[1]); // place new second queen
-        numTries++;
-      }
+      var placedQueens = board.queens;
+      var lastQueen = board.queens[board.queens.length - 1];
+      console.log('lastQueen', lastQueen);
+      placedQueens.push(lastQueen);
+
+      // console.log(board.queens)
+
+          while (board.placeNextQueen()) {
+            board.placeNextQueen();
+          }
+          if (board.getNQueens() === n && !boardsHash[JSON.stringify(board.getBoard())]) {
+            boards.push(board.getBoard());
+            boardsHash[JSON.stringify(board.getBoard())] = 1;
+          }
+          board = new Board(n, i, j); // reset board
+          // board = new Board(n, placedQueens)
+          secondQueen = board.placeNextQueen(); // place new second queen secondQueen[0], secondQueen[1]
+          // numTries++;
+        // }
+
+
     }
   }
-  // console.log(boards)
+  console.log(boards)
   return boards
 };
 
-// console.log(solveNQueens(5))
+// solveNQueens(4)
+
+var fourQueensExpected =
+[ [ '.Q..', '...Q', 'Q...', '..Q.' ],
+  [ '..Q.', 'Q...', '...Q', '.Q..' ] ];
 
 
 var fiveQueensActual =
@@ -128,6 +140,8 @@ var missingBoards =
   [ '....Q', '.Q...', '...Q.', 'Q....', '..Q..' ],
   [ '....Q', '..Q..', 'Q....', '...Q.', '.Q...' ] ];
 
-console.log(JSON.stringify(fiveQueensExpected) === JSON.stringify(solveNQueens(5)))
-var expectedfiltered = fiveQueensExpected.filter(board => !JSON.stringify(fiveQueensActual).includes(JSON.stringify(board)))
-console.log(expectedfiltered)
+
+// console.log(fourQueensExpected.every(board => JSON.stringify(solveNQueens(4)).includes(JSON.stringify(board))))
+console.log(fiveQueensExpected.every(board => solveNQueens(5).includes(board)))
+var fiveNBoardsMissing = fiveQueensExpected.filter(board => !JSON.stringify(fiveQueensActual).includes(JSON.stringify(board)))
+console.log(fiveNBoardsMissing)
